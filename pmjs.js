0032c303d5c0ec10607bb374b8994b929d4987e3
Uowa <uowa905@yahoo.co.jp>
2019-07-22 20:22:16 +0900

    アバターの移動問題を解決した

--- pmjs.js
+++ pmjs.js
@@ -14,18 +14,19 @@ window.onload=function(){
     //あとで消す:座標表示
     document.getElementById("txtX").value = mX;
     document.getElementById("txtY").value = mY;
+  });
 
 
 
     //クリック時に発生するイベント
-    onclick = function() {
+    document.getElementById("graphic").onclick = () => {
       // ここに画面をクリックしたら発生させる処理を記述する
       var avatarX = mX;
       var avatarY = mY;
       var avatar = document.getElementById("avatar");
 
-      document.getElementById("avatar").style.left = avatarX;
-      document.getElementById("avatar").style.top = avatarY;
-     };
-  });
+      avatar.style.position = "absolute";
+      avatar.style.left = avatarX+ "px";
+      avatar.style.top = avatarY+ "px";
+  }
 }
