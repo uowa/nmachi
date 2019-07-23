b1054bc0b9d6581a7c94057122e0e2546fb42805
Uowa <uowa905@yahoo.co.jp>
2019-07-24 02:54:46 +0900

    件のonClickイベントのとこの要らん記述消した
    なんか見返したら、要らん記述あるな・・・　ってなったので、一応直した。

--- pmjs.js
+++ pmjs.js
@@ -34,7 +34,6 @@ var direction_src = new Array("right.png","left.png");
 //クリック時に発生するイベント
 document.getElementById("graphic").onclick = () => {
 // ここに画面をクリックしたら発生させる処理を記述する
-var avatar = document.getElementById("avatar");
 
 if(mX > avatar.style.left){
   document.getElementById("avatar").src = direction_src[0];
@@ -42,11 +41,7 @@ if(mX > avatar.style.left){
   document.getElementById("avatar").src = direction_src[1];
 }
 
-  var avatarX = mX;
-  var avatarY = mY;
-
-
   avatar.style.position = "absolute";
-  avatar.style.left = avatarX+ "px";
-  avatar.style.top = avatarY+ "px";
+  avatar.style.left = mX+ "px";
+  avatar.style.top = mY+ "px";
 }
