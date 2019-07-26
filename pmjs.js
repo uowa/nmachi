5426ec3c134eb631232e6e32d31406c4a659ca5d
Uowa <uowa905@yahoo.co.jp>
2019-07-26 16:44:22 +0900

    とりあえず画像の切り替えを解決
    style.leftが最初はnumberなのに、string型で代入しないといけないっていう、謎仕様が原因だった。

--- pmjs.js
+++ pmjs.js
@@ -1,47 +1,37 @@
 //日付:たまたまソースを見っけたのでとりあえず入れといた、あとでver4の日付と合わせたい
 var day = new Date().toLocaleString();
-document.getElementById("box").innerHTML = day;
-
-
+document.getElementById('box').innerHTML = day;
 
 
 //マウスの座標
 var mX;
 var mY;
-window.onload=function(){
-  //マウス移動時のイベントをBODYタグに登録する
-  document.body.addEventListener("mousemove", function(e){
+window.addEventListener('DOMContentLoaded', function() {
+    document.body.onmousemove = function (e) {
+        mX = e.pageX;
+        mY = e.pageY;
+//マウス移動時のイベントをBODYタグに登録する
+        document.getElementById('txtX').value = mX;
+        document.getElementById('txtY').value = mY;
+    };
+})
 
 
+//クリックした位置にアバターの移動と画像切り替え
+//前のクリック位置との比較で動かしてるけど、あとあと問題になるような気がしなくもない
+var avatarX = 0;
+const direction_src = new Array('right.png', 'left.png');
+document.getElementById('graphic').onclick = function () {
+  //ここにクリックした時の処理を記述
+    if (mX > avatarX) {
+        avatar.src = direction_src[0];
+    } else {
+        avatar.src = direction_src[1];
+    }
 
-    //座標を取得する
-    mX = e.pageX;  //X座標
-    mY = e.pageY;  //Y座標
-    //あとで消す:座標表示
-    document.getElementById("txtX").value = mX;
-    document.getElementById("txtY").value = mY;
-  });
-}
+    avatar.style.position= 'absolute';
+    avatar.style.left = `${mX}px`;
+    avatar.style.top = `${mY}px`;
 
-
-
-
-//？？？？？？？クリックしたら画像を切り替えて、場所を移動するアバターって
-//処理をやりたいんやけど、なんで上手くいかんのかわからんぞ！！！
-//最初の一回しか画像が切り替わってくれん！！
-const direction_src = new Array("right.png","left.png");
-
-//クリック時に発生するイベント
-document.getElementById("graphic").onclick = () => {
-// ここに画面をクリックしたら発生させる処理を記述する
-
-if(mX > avatar.style.left){
-  document.getElementById("avatar").src = direction_src[0];
-}else{
-  document.getElementById("avatar").src = direction_src[1];
-}
-
-  avatar.style.position = "absolute";
-  avatar.style.left = mX+ "px";
-  avatar.style.top = mY+ "px";
-}
+    avatarX = mX;
+  }
